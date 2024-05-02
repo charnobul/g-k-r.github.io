3b50d58d289774aae3e3a1fb0780a885060373dc
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-03 01:36:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 0;
-let initialBonusClaimed = localStorage.getItem('initialBonusClaimed') === 'true';
 
 // Обновляем баланс при загрузке страницы
 document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
@@ -8,13 +7,4 @@ document.getElementById('clicker-button').addEventListener('click', function() {
     balance += 0.001;
     document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
     localStorage.setItem('balance', balance.toFixed(3));
-});
-
-document.getElementById('initial-bonus').addEventListener('click', function() {
-    if (!initialBonusClaimed) {
-        balance += 1; // Добавляем начальный бонус
-        document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
-        localStorage.setItem('balance', balance.toFixed(3));
-        localStorage.setItem('initialBonusClaimed', 'true');
-    }
 });
