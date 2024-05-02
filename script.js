7418b71ff86afc9592fc8bae67371a342f503b1a
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-03 00:37:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let balance = 0;
+let balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 0;
+
+document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
 
 document.getElementById('clicker-button').addEventListener('click', function() {
     balance += 0.001;
     document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
+    localStorage.setItem('balance', balance);
 });
