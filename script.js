b12071f821c7f4baf25d81ad47a167ee4db6ea26
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-02 23:59:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let balance = 0;
+
+document.getElementById('clicker-button').addEventListener('click', function() {
+    balance += 0.001;
+    document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
+});
