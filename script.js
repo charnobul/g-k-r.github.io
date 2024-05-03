b016ae0c1bc90c03427168f7e06cfcf6972b6832
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-03 12:48:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 1;
+let button = document.getElementById('clicker-button');
+let clickSound = new Audio('sounds/2e371cbd1ce9be1.mp3'); // Замените на путь к вашему аудиофайлу
 
 // Обновляем баланс при загрузке страницы
 document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
 
-document.getElementById('clicker-button').addEventListener('click', function() {
+button.addEventListener('touchstart', function() {
+    button.style.transform = 'scale(0.95)';
+});
+
+button.addEventListener('touchend', function() {
+    button.style.transform = 'scale(1)';
+});
+
+button.addEventListener('click', function() {
     balance += 0.001;
     document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
     localStorage.setItem('balance', balance.toFixed(3));
@@ -12,4 +22,7 @@ document.getElementById('clicker-button').addEventListener('click', function() {
     if (window.navigator && typeof window.navigator.vibrate === 'function') {
         window.navigator.vibrate(100);
     }
+
+    // Проигрываем звук
+    clickSound.play();
 });
