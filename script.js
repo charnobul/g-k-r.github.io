ac670b730f90554f7c0cf798d627351d342221e4
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-03 09:56:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 1;
+
+// Обновляем баланс при загрузке страницы
+document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
+
 document.getElementById('clicker-button').addEventListener('click', function() {
     balance += 0.001;
     document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
     localStorage.setItem('balance', balance.toFixed(3));
 
     // Добавляем вибрацию
-    if (navigator.vibrate) { // Проверяем, поддерживает ли браузер Vibration API
+    if (window.navigator && window.navigator.vibrate) { // Проверяем, поддерживает ли браузер Vibration API
         navigator.vibrate(100); // Вибрация длительностью 100 миллисекунд
     }
 });
