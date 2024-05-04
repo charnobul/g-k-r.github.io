d24e747bba3b7e2e4c4786e7ba2d69900f58851d
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-04 20:30:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let shopButton = document.getElementById('buy-farm');
 let clickSound = new Audio('sounds/2e371cbd1ce9be1.mp3'); // Замените на путь к вашему аудиофайлу
 
 // Обновляем баланс и количество ферм при загрузке страницы
-document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
+document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
 document.getElementById('farm-counter').textContent = farms;
 
 button.addEventListener('touchstart', function() {
@@ -17,9 +17,9 @@ button.addEventListener('touchend', function() {
 });
 
 button.addEventListener('click', function() {
-    balance += 0.001 * farms;
-    document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
-    localStorage.setItem('balance', balance.toFixed(3));
+    balance += 0.01 * farms;
+    document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
+    localStorage.setItem('balance', balance.toFixed(2));
 
     // Добавляем вибрацию
     if (window.navigator && typeof window.navigator.vibrate === 'function') {
@@ -31,12 +31,12 @@ button.addEventListener('click', function() {
 });
 
 shopButton.addEventListener('click', function() {
-    if (balance >= 0.100) {
-        balance -= 0.100;
+    if (balance >= 1) {
+        balance -= 1;
         farms += 1;
-        document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
+        document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
         document.getElementById('farm-counter').textContent = farms;
-        localStorage.setItem('balance', balance.toFixed(3));
+        localStorage.setItem('balance', balance.toFixed(2));
         localStorage.setItem('farms', farms);
     } else {
         alert('Недостаточно средств для покупки фермы');
@@ -45,7 +45,7 @@ shopButton.addEventListener('click', function() {
 
 // Обновляем баланс каждую секунду
 setInterval(function() {
-    balance += 0.010 * farms;
-    document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(3) + ' гривен';
-    localStorage.setItem('balance', balance.toFixed(3));
+    balance += 0.01 * farms;
+    document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
+    localStorage.setItem('balance', balance.toFixed(2));
 }, 1000);
