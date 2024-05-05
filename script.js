988f3b52a21e5502f782e3e5d01b5a09c8e3b450
charnobul <168744540+charnobul@users.noreply.github.com>
2024-05-05 13:55:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 1;
-let farms = localStorage.getItem('farms') ? JSON.parse(localStorage.getItem('farms')) : [0, 0];
+let farms = localStorage.getItem('farms') ? JSON.parse(localStorage.getItem('farms')) : [0, 0, 0];
 let button = document.getElementById('clicker-button');
-let shopButtons = document.getElementsByClassName('buy-farm');
+let shopButton = document.getElementById('toggle-shop');
+let buyButtons = document.getElementsByClassName('buy-farm');
 let resetButton = document.getElementById('reset-button');
 let clickSound = new Audio('sounds/2e371cbd1ce9be1.mp3'); // Замените на путь к вашему аудиофайлу
 
@@ -34,10 +35,21 @@ button.addEventListener('click', function() {
     clickSound.play();
 });
 
-for (let i = 0; i < shopButtons.length; i++) {
-    shopButtons[i].addEventListener('click', function() {
-        let cost = i === 0 ? 1 : 100;
-        let income = i === 0 ? 0.01 : 1;
+shopButton.addEventListener('click', function() {
+    let shop = document.getElementById('shop');
+    if (shop.style.display === 'none') {
+        shop.style.display = 'flex';
+        shopButton.textContent = '✖️ Закрыть магазин';
+    } else {
+        shop.style.display = 'none';
+        shopButton.textContent = 'Магазин ферм';
+    }
+});
+
+for (let i = 0; i < buyButtons.length; i++) {
+    buyButtons[i].addEventListener('click', function() {
+        let cost = i === 0 ? 1 : i === 1 ? 100 : 10000;
+        let income = i === 0 ? 0.01 : i === 1 ? 1 : 100;
         if (balance >= cost) {
             balance -= cost;
             farms[i] += 1;
@@ -53,7 +65,7 @@ for (let i = 0; i < shopButtons.length; i++) {
 
 // Обновляем баланс каждую секунду
 setInterval(function() {
-    balance += 0.01 * farms[0] + 1 * farms[1];
+    balance += 0.01 * farms[0] + 1 * farms[1] + 100 * farms[2];
     balance = Math.min(balance, 1e308); // Ограничиваем баланс
     document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
     localStorage.setItem('balance', balance.toFixed(2));
@@ -63,7 +75,7 @@ setInterval(function() {
 resetButton.addEventListener('click', function() {
     if (confirm('Вы уверены, что хотите обнулить всё?')) {
         balance = 1;
-        farms = [0, 0];
+        farms = [0, 0, 0];
         document.getElementById('balance').textContent = 'Баланс: ' + balance.toFixed(2) + ' гривен';
         for (let i = 0; i < farmCounters.length; i++) {
             farmCounters[i].textContent = farms[i];
